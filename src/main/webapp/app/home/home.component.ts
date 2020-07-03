c0db9622625362296e97ab0602faa47f0cbeb976
RohitLab <rohit.gaikwad127@gmail.com>
2020-07-03 13:40:22 +0530

    basics all done

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-home',
@@ -13,11 +14,65 @@ import { Account } from 'app/core/user/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
+  // form: any;
+  // submitted: boolean = false;
+  // powers: Array <string> =  [
+  //   '09:00 AM - 10:00 AM',
+  //   '10:00 AM - 11:00 AM',
+  //   '11:00 AM - 12:00 PM',
+  //   '12:00 PM - 01:00 PM',
+  //   '01:00 PM - 02:00 PM',
+  //   '03:00 PM - 04:00 PM',
+  //   '04:00 PM - 05:00 PM',
+  //   '05:00 PM - 06:00 PM',
+  //   '06:00 PM - 07:00 PM',
+  // ];
+  // form: any;
+  // submitted: boolean = false;
+  // powers: Array <string> =  [
+  //   '09:00 AM - 10:00 AM',
+  //   '10:00 AM - 11:00 AM',
+  //   '11:00 AM - 12:00 PM',
+  //   '12:00 PM - 01:00 PM',
+  //   '01:00 PM - 02:00 PM',
+  //   '03:00 PM - 04:00 PM',
+  //   '04:00 PM - 05:00 PM',
+  //   '05:00 PM - 06:00 PM',
+  //   '06:00 PM - 07:00 PM',
+  // ];
+  form: any;
+  powers: any;
+  submitted = false;
+  minDate = undefined;
+  maxDate = undefined;
 
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
+  constructor(private accountService: AccountService, private loginModalService: LoginModalService, private config: NgbDatepickerConfig) {}
 
   ngOnInit(): void {
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
+    this.powers = [
+      '09:00 AM - 10:00 AM',
+      '10:00 AM - 11:00 AM',
+      '11:00 AM - 12:00 PM',
+      '12:00 PM - 01:00 PM',
+      '01:00 PM - 02:00 PM',
+      '03:00 PM - 04:00 PM',
+      '04:00 PM - 05:00 PM',
+      '05:00 PM - 06:00 PM',
+      '06:00 PM - 07:00 PM',
+    ];
+    const current = new Date();
+    this.minDate = {
+      year: current.getFullYear(),
+      month: current.getMonth() + 1,
+      day: current.getDate(),
+    };
+
+    this.maxDate = {
+      year: current.getFullYear(),
+      month: current.getMonth() + 1,
+      day: current.getDate() + 7,
+    };
   }
 
   isAuthenticated(): boolean {
@@ -32,5 +87,9 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
+  }
+  onSubmit(form: any): any {
+    this.submitted = true;
+    this.form = form;
   }
 }
